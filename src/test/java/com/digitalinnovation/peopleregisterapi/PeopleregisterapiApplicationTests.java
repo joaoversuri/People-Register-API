0548437cca6cbfa0ec84098ccc26ee548003d73b
joaoversuri <joaopaulo.db.versuri@gmail.com>
2022-01-31 21:41:10 -0300

    Heroku fix

---
+++ src/test/java/com/digitalinnovation/peopleregisterapi/PeopleregisterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.peopleregisterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleregisterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
